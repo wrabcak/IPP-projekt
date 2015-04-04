2fc078990c70c28b195f57c967f838be54904b34
Lukas Vrabec <lvrabec@redhat.com>
2015-04-05 00:08:36 +0200

    Parsing

--- DKA/parser.py
+++ DKA/parser.py
@@ -2,6 +2,16 @@
 
 import re
 
+class Rule:
+    fromState = False
+    symbol = False
+    toState = False
+
+    def __init__(self,fromState,symbol,toState):
+        self.fromState = fromState
+        self.symbol = symbol
+        self.toState = toState
+
 class Parser:
     inputFile = False
     caseInsensitive = False
@@ -9,7 +19,9 @@ class Parser:
 
     __fsmStates = set()
     __fsmAlphabet = set()
-    __fsmRules = {}
+    __fsmRules = set()
+    __fsmInitState = False
+    __fsmFinishStates = set()
 
     def __init__(self,inputFile,caseInsensitive):
         self.inputFile = inputFile
@@ -57,6 +69,29 @@ class Parser:
 
         raise Exception(40)
 
+    def __getFinishStates(self,index):
+        fileLength = self.inputFileLength
+
+        while(index < fileLength):
+            index = self.__skip(index)
+            match = re.match('^([a-zA-Z]([a-zA-Z0-9_]*[a-zA-Z0-9])?)',self.inputFile[index:])
+            if not match:
+                raise Exception(40)
+
+            finishState = match.group(0)
+
+            self.__fsmFinishStates.add(finishState)
+            index = index + len(finishState)
+
+            if self.inputFile[index] == ',':
+                index = index + 1
+                continue
+            index = self.__skip(index)
+            if self.inputFile[index] == '}':
+                return index
+
+        raise Exception(40)
+
     def __parseInputSymbol(self,symbol):
         if symbol[0] == "'" and symbol[2] == "'":
             return symbol[1]
@@ -84,6 +119,91 @@ class Parser:
                 return index
 
         raise Exception(40)
+
+    def __getRules(self,index):
+        fileLength = self.inputFileLength
+
+        fromState = None
+        toState = None
+        symbol = None
+
+        while(index < fileLength):
+            index = self.__skip(index)
+            match = re.match('^([a-zA-Z]([a-zA-Z0-9_]*[a-zA-Z0-9])?)',self.inputFile[index:])
+            if not match:
+                raise Exception(40)
+
+            fromState = match.group(0)
+
+            if fromState not in self.__fsmStates:
+                raise Exception(41)
+
+            index = index + len(fromState)
+            index = self.__skip(index)
+
+            if self.inputFile[index:index+2] == '->':
+                symbol = 'eps'
+            else:
+                inputSymbol = self.inputFile[index:index+3]
+                symbol = self.__parseInputSymbol(inputSymbol)
+                index = index + len(inputSymbol)
+
+            if symbol != 'eps' and symbol not in self.__fsmAlphabet:
+                raise Exception(41)
+
+            index = self.__skip(index)
+
+            if self.inputFile[index:index+2] == '->':
+                 index = index + 2
+                 index = self.__skip(index)
+                 match = re.match('^([a-zA-Z]([a-zA-Z0-9_]*[a-zA-Z0-9])?)',self.inputFile[index:])
+                 if not match:
+                     raise Exception(40)
+
+                 toState = match.group(0)
+
+                 if toState not in self.__fsmStates:
+                     raise Exception(41)
+
+                 index = index + len(toState)
+                 index = self.__skip(index)
+
+            else:
+                raise Exception(41)
+
+            actualRule = Rule(fromState,symbol,toState)
+
+            self.__fsmRules.add(actualRule)
+
+            index = self.__skip(index)
+
+            if self.inputFile[index] == ',':
+                index = index + 1
+                continue
+
+            if self.inputFile[index] == '}':
+                return index
+
+    def __getInitState(self,index):
+        index = self.__skip(index)
+
+        match = re.match('^([a-zA-Z]([a-zA-Z0-9_]*[a-zA-Z0-9])?)',self.inputFile[index:])
+        if not match:
+            raise Exception(40)
+
+        initState = match.group(0)
+
+        if initState not in self.__fsmStates:
+            raise Exception(41)
+
+        index = index + len(initState)
+        index = self.__skip(index)
+
+        if self.inputFile[index] != ',':
+            raise Exception(40)
+
+        self.__fsmInitState = initState
+        return index
 
     def parseFsm(self):
         index = 0
@@ -123,5 +243,50 @@ class Parser:
         else:
             raise Exception(40)
 
+        index = index + 1
+        index = self.__skip(index)
+        if self.inputFile[index] != ',':
+            raise Exception(40)
+        index = index + 1
+        index = self.__skip(index)
+
+        if self.inputFile[index] == '{':
+            index = index +1
+            index = self.__skip(index)
+            if self.inputFile[index] == '}':
+                raise Exception(41)
+            else:
+                index = self.__getRules(index)
+        else:
+            raise Exception(40)
+
+        index = index + 1
+        index = self.__skip(index)
+        if self.inputFile[index] != ',':
+            raise Exception(40)
+        index = index + 1
+        index = self.__skip(index)
+
+        index = self.__getInitState(index)
+
+        index = index + 1
+        index = self.__skip(index)
+
+        if self.inputFile[index] == '{':
+            index = index +1
+            index = self.__skip(index)
+            if self.inputFile[index] == '}':
+                index = index + 1
+            else:
+                index = self.__getFinishStates(index)
+        else:
+            raise Exception(40)
+
+
+
         print(self.__fsmStates)
         print(self.__fsmAlphabet)
+        for test in self.__fsmRules:
+            print(test.fromState, ' ', test.symbol, ' ', test.toState)
+        print(self.__fsmInitState)
+        print(self.__fsmFinishStates)
